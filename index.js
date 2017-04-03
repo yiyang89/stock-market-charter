0961f6e0e9abad8b2fb62b61576b7789e8857257
Yiyang Kok <yiyangkok@gmail.com>
2017-04-03 09:55:34 -0700

    initial changes

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 var express = require('express');
 var app = express();
-var validator = require('./scripts/validator.js');
-
-// User Story: I can pass a string as a parameter, and it will check to see whether that string contains
-//            either a unix timestamp or a natural language date (example: January 1, 2016).
-// User Story: If it does, it returns both the Unix timestamp and the natural language form of that date.
-// User Story: If it does not contain a date or Unix timestamp, it returns null for those properties.
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -18,14 +12,12 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/:CONTENT', function(request, response) {
-  console.log(request.params);
-  // parse the params.
-  // examples:
-  // [address]/December%2015,%202015
-  // [address]/1450137600
-  response.send(validator.process(request.params.CONTENT));
-
+app.get('/api/whoami/', function(request, response) {
+  var respObject = {"ipaddress": null, "language":null, "software": null};
+  respObject.ipaddress = '';
+  respObject.language = request.headers;
+  respObject.software = '';
+  response.send(respObject);
 });
 
 app.listen(app.get('port'), function() {
