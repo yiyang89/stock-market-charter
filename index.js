ddaa93a71ec73e6ec73437b807cdbb815bc04252
Yiyang Kok <yiyangkok@gmail.com>
2017-04-05 13:51:44 -0700

    initial commit

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ var app = express();
 var multer = require('multer');
 var upload = multer({dest:'./uploads/'});
 var fs = require('fs');
+var passport = require('passport');
+// Each type of passport plugin will require its specific oauth strategy.
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+// Need to create credentials on https://console.developers.google.com/
+// MOVE THESE INTO ENV VARIABLES BEFORE DEPLOYING.
+var GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+var GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+passport.use(new GoogleStrategy({
+    clientID:     GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://oauth-template-decky.herokuapp.com/auth/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -22,6 +42,20 @@ app.post('/file-size', upload.single('file'), function(request, response, next) 
   // delete the temp file
   fs.unlinkSync('./uploads/' + request.file.filename);
   response.send({'size':size});
+});
+
+// auth code from https://c9.io/barberboy/passport-google-oauth2-example
+// send auth request to google
+app.get('/auth/google', function(request, response) {
+  // Do some google auth stuff here
+  passport.authenticate('google', { scope: ['email profile'] });
+});
+
+// get auth callback from google
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: 'pages/fail' }), function (request, response) {
+  console.log("retrieved a callback! Contents:");
+  console.log(JSON.stringify(response));
+  response.render('pages/success');
 });
 
 app.listen(app.get('port'), function() {
