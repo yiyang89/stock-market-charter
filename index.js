c2235c703ee493bd9693d4256b9dfcba4008b47a
Yiyang Kok <yiyangkok@gmail.com>
2017-04-06 16:04:06 -0700

    returned response after auth redirect

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var fs = require('fs');
 var passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
 // Each type of passport plugin will require its specific oauth strategy.
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 // Need to create credentials on https://console.developers.google.com/
@@ -12,8 +15,8 @@ var GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://oauth-template-decky.herokuapp.com/auth/google/callback",
-    // callbackURL:"http://localhost:5000/auth/google/callback",
+    // callbackURL: "http://oauth-template-decky.herokuapp.com/auth/google/callback",
+    callbackURL:"http://localhost:5000/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
@@ -21,42 +24,45 @@ passport.use(new GoogleStrategy({
     console.log("PROFILE: " + profile);
     console.log("ACCESS TOKEN: " + JSON.stringify(accessToken));
     console.log("REFRESH TOKEN: " + JSON.stringify(refreshToken));
-    // done is what is returned to the user.
-    return done("Successfully verified!");
+    console.log(done);
+    return done(null, profile);
   }
 ));
+
+// Serializing is part of session handling
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.sendFile(path.join(__dirname+'/views/pages/index.html'));
 });
 
 // auth code from https://c9.io/barberboy/passport-google-oauth2-example
 // send auth request to google
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email profile'] }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['email profile'] }
+  ))
 
 // get auth callback from google
 app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/failLogin' }),
-    function (request, response) {
-    // The response that comes back is the content of the "return done('CONTENT'); in the strategy definition"
-    // console.log("retrieved a callback! Contents:");
-    // console.log(JSON.stringify(response));
-    // response.redirect('/');
-    // response.render('pages/success');
-});
+  passport.authenticate('google'),
+  function(request, response) {
+    console.log("finished authentication");
+    if (request.user) {
+      response.set({'Content-Type':'application/json'});
+      response.send('someMethod' + '('+ JSON.stringify(request.user) + ');');
+    } else { response.jsonp(401); }
+  });
 
-// Failed to login.
-app.get('/failLogin', function (request, response) {
-  response.render(pages/fail);
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ public/scripts/voting.js
@@ -0,0 +1,60 @@
+// Globals:
+var host = "http://192.168.0.53:5000";
+var user = null;
+
+console.log("voting.js loaded successfully");
+
+// Login area class
+var LoginArea = React.createClass({
+  render: function() {
+    // Return either a button or a welcome message based on global being set.
+    if (user) {
+      return <p> Welcome [USERNAME GOES HERE]! </p>;
+    } else {
+      return (<button onClick={processLogin} id="loginButton" className="btn btn-primary waves-effect waves-light loginbtn">Google Login</button>);
+    }
+  }
+});
+
+// Front end rendering and logic
+// Use React and jQuery
+
+// Render the default login area
+ReactDOM.render(<LoginArea />,document.getElementById('login'));
+
+// Render the default display area
+
+// Request to backend for login
+function someMethod(response) {
+  console.log(response);
+}
+
+function processLogin() {
+  $.ajax({
+    url: host+'/auth/google?callback=?',
+    dataType: 'jsonp',
+    jsonp: 'callback',
+    success: function(data) {
+      console.log('success');
+      console.log(JSON.stringify(data));
+    }
+  });
+}
+
+// Request to backend for initial polls.
+
+// Request to backend for single poll.
+
+// Request to backend for poll creation
+
+// Request to backend for poll modification
+
+// Request to backend for poll deletion
+
+// Request to backend to vote
+
+// Render login area
+
+
+// Main display area class (REACT)
+// $("#loginButton").on("click", processLogin());
